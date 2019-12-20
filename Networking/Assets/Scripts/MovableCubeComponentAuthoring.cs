2b9f38827367f911843f2cdea30b5ee02977876e
Cort Stratton <cort@unity3d.com>
2019-12-20 15:25:35 -0800

    Manually generate MovableCubeComponent to work around GenerateAuthoringComponent bug

---
+++ Networking/Assets/Scripts/MovableCubeComponentAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+public struct MovableCubeComponent : IComponentData
+{
+    [GhostDefaultField]
+    public int PlayerId;
+}
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class MovableCubeComponentAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int playerId;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new MovableCubeComponent{PlayerId = playerId});
+    }
+}
