ede6c69649633966ed35b123ab99cfa44737d5f0
Aria Bonczek <aria.bonczek@gmail.com>
2019-12-20 14:57:23 -0800

    Initial commit

---
+++ Networking/Assets/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
+using Unity.Burst;
+using UnityEngine;
+
+// Control system updating in the default world
+[UpdateInWorld(UpdateInWorld.TargetWorld.Default)]
+public class GameInitSystem : ComponentSystem
+{
+    // Singleton component to trigger connections once from a control system
+    struct InitGameComponent : IComponentData
+    {
+    }
+    protected override void OnCreate()
+    {
+        RequireSingletonForUpdate<InitGameComponent>();
+        // Create singleton, require singleton for update so system runs once
+        EntityManager.CreateEntity(typeof(InitGameComponent));
+    }
+
+    protected override void OnUpdate()
+    {
+        // Destroy singleton to prevent system from running again
+        EntityManager.DestroyEntity(GetSingletonEntity<InitGameComponent>());
+        foreach (var world in World.AllWorlds)
+        {
+            var network = world.GetExistingSystem<NetworkStreamReceiveSystem>();
+            if (world.GetExistingSystem<ClientSimulationSystemGroup>() != null)
+            {
+                // Client worlds automatically connect to localhost
+                NetworkEndPoint ep = NetworkEndPoint.AnyIpv4;
+                ep.Port = 7979;
+                network.Connect(ep);
+            }
+#if UNITY_EDITOR
+            else if (world.GetExistingSystem<ServerSimulationSystemGroup>() != null)
+            {
+                // Server world automatically listens for connections from any host
+                NetworkEndPoint ep = NetworkEndPoint.AnyIpv4;
+                ep.Port = 7979;
+
+                var strHostName = Dns.GetHostName();
+                IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+                IPAddress[] addr = ipEntry.AddressList;
+
+                for (int i = 0; i < addr.Length; ++i)
+                {
+                    Debug.Log($"{addr[i].ToString()}");
+                }
+                network.Listen(ep);
+            }
+#endif
+        }
+    }
+}
+
+// The system that makes the RPC request component transfer
+public class GoInGameRequestSystem : RpcCommandRequestSystem<GoInGameRequest>
+{
+}
+
+[BurstCompile]
+public struct GoInGameRequest : IRpcCommand
+{
+    public void Deserialize(DataStreamReader reader, ref DataStreamReader.Context ctx)
+    {
+    }
+
+    public void Serialize(DataStreamWriter writer)
+    {
+    }
+    [BurstCompile]
+    private static void InvokeExecute(ref RpcExecutor.Parameters parameters)
+    {
+        RpcExecutor.ExecuteCreateRequestComponent<GoInGameRequest>(ref parameters);
+    }
+
+    public PortableFunctionPointer<RpcExecutor.ExecuteDelegate> CompileExecute()
+    {
+        return new PortableFunctionPointer<RpcExecutor.ExecuteDelegate>(InvokeExecute);
+    }
+}
